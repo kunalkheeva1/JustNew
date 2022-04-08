467f154979382c42b4ed8ed4dd345e23fd9cfc92
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-08 02:57:14 -0700

    kk

--- src/JustNew.java
+++ src/JustNew.java
@@ -1,3 +1,16 @@
 public class JustNew {
-    
+    public static void getInt() {
+        int kk = Integer.parseInt("1");
+        System.out.println(kk);
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            getInt();
+        } catch (Exception e) {
+            System.out.println("Naw man it aint correct move!");
+        }
+
+    }
 }
