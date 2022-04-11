76f57a2a4bb51f8eb962220360b38ce8d3786852
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-10 22:41:11 -0700

    kk

--- src/JustNew.java
+++ src/JustNew.java
@@ -1,25 +1,13 @@
 import java.util.Scanner;
 
 public class JustNew {
-    static int age;
-    static Scanner sc = new Scanner(System.in);
-
-    static void kk() {
-        System.out.println("Enter your age: ");
-        age = sc.nextInt();
-        if (age < 18) {
-            System.out.println("Your are not adult go fuck yourself");
-        } else if (age >= 18){
-        System.out.println("Hell yea we got it man!");
-    }
-
-}
     public static void main(String[] args) {
-        JustNew obj = new JustNew();
-        obj.kk();
-      for(int i = 0; i<age; i++){
-          obj.kk();
-      }
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("odd");
+        }
     }
 }
