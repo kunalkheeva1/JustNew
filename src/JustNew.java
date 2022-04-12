9518752bf9da7162552750a536c78f1b5eb39edc
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-11 22:11:15 -0700

    kk

--- src/JustNew.java
+++ src/JustNew.java
@@ -2,18 +2,8 @@ import java.util.ArrayList;
 
 public class JustNew{
     public static void main(String[] args) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i = 1; i<=5; i++)
-            arr.add(i);
-        System.out.println(arr);
-        arr.add(1,23);
-        System.out.println(arr);
-        arr.set(1,2);
-        System.out.println(arr);
-        arr.remove(1);
-        System.out.println(arr);
-        for (int i= 0; i< arr.size();i++ )
-            System.out.println(arr.get(i));
-
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("Kunal");
+        arr.add("Kheeva");
     }
 }
