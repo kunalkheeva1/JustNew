151392d742510df1464948d6052097df3c719a04
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-11 22:52:58 -0700

    kk

--- src/JustNew.java
+++ src/JustNew.java
@@ -1,9 +1,20 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class JustNew{
     public static void main(String[] args) {
         ArrayList<String> arr = new ArrayList<>();
         arr.add("Kunal");
         arr.add("Kheeva");
+        System.out.println(arr);
+        for (int i = 0; i< arr.size(); i++)
+            System.out.println(" "+arr.get(i));
+        arr.remove(1);
+        System.out.println(arr);
+        arr.add(1,"Kheeva");
+        System.out.println(arr);
+        arr.set(0, "Nav");
+        Collections.sort(arr);
+
     }
 }
