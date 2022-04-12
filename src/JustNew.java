164a15cc8fb5cabc692cb8a879419a52e7d371ee
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-12 01:12:35 -0700

    odd number

--- src/JustNew.java
+++ src/JustNew.java
@@ -3,10 +3,19 @@ import java.util.Scanner;
 public class JustNew {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        double input= sc.nextDouble();
-      for(double i=0; i<input; i++){
-          System.out.println(2*i+1);
+        int input = sc.nextInt();
 
-        }
+      do{
+          int marks = sc.nextInt();
+          if(marks>=90&& marks<=100){
+              System.out.println("You got an A");
+          }else if(marks>=80 && marks<90){
+              System.out.println("Your got a B");
+          }else if (marks>=60 && marks<70){
+              System.out.println("You got a C");
+          }else{
+              System.out.println("You failed");
+          }
+      }
     }
 }
