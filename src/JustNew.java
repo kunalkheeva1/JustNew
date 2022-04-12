9f7ef6b62ba6da531c27ac9946d19c02ca5f4d69
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-12 00:20:59 -0700

    kk

--- src/JustNew.java
+++ src/JustNew.java
@@ -4,15 +4,10 @@ public class JustNew {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         double input= sc.nextDouble();
-        double sum=0;
+        double sum;
         for(int i = 1; i<=100; i++){
             sum= input+i;
             System.out.println(sum);
-
         }
-
-
-
-
     }
 }
