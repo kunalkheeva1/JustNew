43b1afc105070a228092bab168c73a2d4ca74f0a
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-11 21:55:04 -0700

    kk

---
+++ src/JustNew.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class JustNew{
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 1; i<=5; i++)
+            arr.add(i);
+        System.out.println(arr);
+    }
+}
