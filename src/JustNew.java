132aee2eede37e26b565b3db4938a3821044fbd2
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-11 23:51:30 -0700

    kk

--- src/JustNew.java
+++ src/JustNew.java
@@ -1,5 +1,12 @@
 public class JustNew {
     public static void main(String[] args) {
+    int i = 0;
+    do{
+        System.out.println("Hello World");
+        i++;
+    }while(i<10);
+
+
 
     }
 }
