3ac5ad669f2a663271ac546fa97bb010e661ebfd
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-13 23:10:14 -0700

    modulus

--- src/JustNew.java
+++ src/JustNew.java
@@ -1,14 +1,20 @@
 public class JustNew{
     public static void main(String[] args) {
+        int n = 4;
+        int k;
 
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= 5; j++) {
-                if (i == 1 || j == 1 || i == 4 || j == 5) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
+        for (int i =1; i<=n; i++) {
+            for (int j=1; j<=i; j++) {
+                int sum = i+j;
+                if(sum%2==0) {
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
                 }
             }System.out.println();
+
         }
+
+
     }
 }
