ecbe7b1432333fd7989b2b34b2a6889f74caad51
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-13 21:52:03 -0700

    nested loop

--- src/JustNew.java
+++ src/JustNew.java
@@ -1,7 +1,10 @@
 public class JustNew{
     public static void main(String[] args) {
-        for(int i = 0; i<=4; i++){
+        for(int i = 1; i<=4; i++){
+            for(int j =1; j<=5; j++){
             System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 }
