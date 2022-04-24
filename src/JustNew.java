dfa81091e7b1e1b7eb0e01c3cdcf9141204d4a52
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-23 20:59:13 -0700

    new commit

--- src/JustNew.java
+++ src/JustNew.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class JustNew {
     public static double kunal(){
          Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your first number: ");
          double a= scanner.nextDouble();
+        System.out.println("Please enter your second number: ");
          double b = scanner.nextDouble();
          double sum = a+b;
-         return sum ;
+        System.out.println("Here you go: ");
+        System.out.println(sum);
+         return sum;
     }
     public static void main(String[] args) {
         kunal();
+
     }
 }
