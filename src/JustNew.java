a86bd1e458a9f65c1b0410e56b7da633b372bc2b
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-23 20:56:15 -0700

    modulus

--- src/JustNew.java
+++ src/JustNew.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
 public class JustNew {
-
+    public static double kunal(){
+         Scanner scanner = new Scanner(System.in);
+         double a= scanner.nextDouble();
+         double b = scanner.nextDouble();
+         double sum = a+b;
+         return sum ;
+    }
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println(name);
-
+        kunal();
     }
 }
