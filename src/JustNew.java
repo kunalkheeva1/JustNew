7e8687e2eabe9906963d18708b87a44f792124bc
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-25 00:48:40 -0700

    factorial

--- src/JustNew.java
+++ src/JustNew.java
@@ -1,25 +1,13 @@
 import java.util.Scanner;
-
 public class JustNew {
-    static void kk(){
-        System.out.println("Please enter your number: ");
-        Scanner sc = new Scanner(System.in);
-        double input = sc.nextDouble();
-        double n=input;
-        if(n%2==0){
-            System.out.println("It is an even number!");
-        }
-        else{
-            System.out.println("It is an odd number");
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        for(int i=0; i<m; m++){
+            for(int j=0; j<n; j++){
+                System.out.println("Kunal");
+            }
         }
     }
-    public static void main(String[] args) {
-       int i=0;
-       do{
-           kk();
-       }while(i<=i);{
-            System.out.println("end loop");
-            i++;
-        }
-        }
 }
