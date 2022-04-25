275f05192aa2b9f57b9152b43c8f4c170406c70a
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-25 00:13:12 -0700

    factorial

--- src/JustNew.java
+++ src/JustNew.java
@@ -1,22 +1,25 @@
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Scanner;
 
 public class JustNew {
-
-    static void factorial(double n) {
-        double fact = 1;
-
-        for(double i=n; i>=1; i--){
-            fact = fact*i;
-        }
-        System.out.println(fact);
-    }
-
-    public static void main(String[] args) {
+    static void kk(){
+        System.out.println("Please enter your number: ");
         Scanner sc = new Scanner(System.in);
-        double n = sc.nextDouble();
-        factorial(n);
-
+        double input = sc.nextDouble();
+        double n=input;
+        if(n%2==0){
+            System.out.println("It is an even number!");
+        }
+        else{
+            System.out.println("It is an odd number");
+        }
     }
+    public static void main(String[] args) {
+       int i=0;
+       do{
+           kk();
+       }while(i<=i);{
+            System.out.println("end loop");
+            i++;
+        }
+        }
 }
