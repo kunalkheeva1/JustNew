5ed427067772b533e3eba5bbce19e78751f0babf
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-30 22:38:19 -0700

    reversed String builder in the loop

--- src/JustNew.java
+++ src/JustNew.java
@@ -1,13 +1,17 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class JustNew {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in); // Scanner to get the input
-        System.out.println("Please Enter your name: ");
-        String kk = sc.nextLine(); //gets the input
-        System.out.print("Your entry ");
-        for(int i=0; i<kk.length(); i++){
-            System.out.print( kk.charAt(i)); //Printing the name which has been inputed
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your input to be reversed: ");
+        StringBuilder kk = new StringBuilder(sc.nextLine());
+        for(int i=0; i<kk.length()/2; i++){
+
+            char first= kk.charAt(i);
+            char last= kk.charAt(kk.length()-1-i);
+            kk.setCharAt(i,last);
+            kk.setCharAt(kk.length()-1-i,first);
         }
-        System.out.print(" has been registered!!");
+        System.out.println("Reversed input: "+ "\n"+kk);
     }
 }
