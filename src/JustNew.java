c939978ff96895e7cd7619da4027e1dcaa9237f7
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-30 22:39:00 -0700

    reversed String builder in the loop with stored indexes.

--- src/JustNew.java
+++ src/JustNew.java
@@ -1,17 +1,7 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
 public class JustNew {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your input to be reversed: ");
-        StringBuilder kk = new StringBuilder(sc.nextLine());
-        for(int i=0; i<kk.length()/2; i++){
-
-            char first= kk.charAt(i);
-            char last= kk.charAt(kk.length()-1-i);
-            kk.setCharAt(i,last);
-            kk.setCharAt(kk.length()-1-i,first);
-        }
-        System.out.println("Reversed input: "+ "\n"+kk);
+        int a =10;
+        a++; //checking the operators
+        System.out.println(a);
     }
 }
