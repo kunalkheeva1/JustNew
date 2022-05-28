aa60db5b52f34acdb990b168ab87f19247527c0c
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-28 00:52:57 -0700

    asccii

--- src/JustNew.java
+++ src/JustNew.java
@@ -1,7 +1,7 @@
-public class JustNew {
+public class JustNew{
     public static void main(String[] args) {
-        int b=0,a =10;
-        b=a; //checking the operators
-        System.out.println(a+" "+b);
+        char kk= 1;
+        System.out.println(kk);
+
     }
 }
